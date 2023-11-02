0d9b1f932f39f88ade1c733fb45aabc5b3e06684
Hong Soon Uk <tnsdnr1809@gmail.com>
2023-11-03 04:35:22 +0900

    pikachu 객체 생성(왼쪽, 오른쪽 캐릭터 생성)

--- StartGame/control_pikachu.py
+++ StartGame/control_pikachu.py
@@ -1,6 +1,7 @@
 from pico2d import *
 import random
 
+
 class P_Map:
     def __init__(self):
         self.image = load_image('map.png')
@@ -18,7 +19,7 @@ class Cloud():
         self.frame = random.randint(0, 1)
         self.speed = random.randint(1, 3)
         self.image = load_image('cloud.png')
-        self.direction = 2
+        self.direction = 1
 
     def update(self):
         self.frame = (self.frame + 1) % 1
@@ -36,35 +37,72 @@ class Cloud():
 
 class Pikachu:
     def __init__(self):
-        self.x, self.y = 40, 120
+        self.x, self.y = 80, 120
         self.frame = 0
-        self.direction = 1
+        self.direction = 0
         self.image = load_image('walk_animation.png')
 
     def update(self):
         self.frame = (self.frame + 1) % 5
-        self.x += 5 * self.direction
+        self.x += 10 * self.direction
 
         if self.x <= 40:
-            self.direction = 1
-
+            self.x = 40
         elif self.x >= 350:
-            self.direction = 0
-
+            self.x = 350
 
     def draw(self):
-        self.image.clip_draw(self.frame * 110, 0, 110, 110, self.x, self.y)
+        self.image.clip_draw(int(self.frame) * 110, 0, 110, 110, self.x, self.y)
+
+
+class Pikachu_Right:
+    def __init__(self):
+        self.x, self.y = 700,  120
+        self.frame = 0
+        self.direction = 0
+        self.image = load_image('walk_animation_right.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 5
+        self.x += 10 * self.direction
+
+        if self.x >= 750:
+            self.x = 750
+        elif self.x <= 440:
+            self.x = 440
+
+    def draw(self):
+        self.image.clip_draw(int(self.frame) * 113, 0, 110, 113, self.x, self.y)
 
 
 def handle_events():
     global running
+    global pikachu
+    global pikachu_right
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
-
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_g: # 오른쪽 방향
+                pikachu.direction += 1
+            elif event.key == SDLK_d: # 왼쪽 방향
+                pikachu.direction -= 1
+            elif event.key == SDLK_RIGHT:
+                pikachu_right.direction += 1
+            elif event.key == SDLK_LEFT:
+                pikachu_right.direction -= 1
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_g: # 오른쪽 방향
+                pikachu.direction -= 1
+            elif event.key == SDLK_d: # 왼쪽 방향
+                pikachu.direction += 1
+            elif event.key == SDLK_RIGHT:
+                pikachu_right.direction -= 1
+            elif event.key == SDLK_LEFT:
+                pikachu_right.direction += 1
 
 def reset_world():
     global running
@@ -72,6 +110,7 @@ def reset_world():
     global p_map
     global cloud
     global pikachu
+    global pikachu_right
 
     running = True
     world = []
@@ -84,6 +123,9 @@ def reset_world():
 
     pikachu = Pikachu()
     world.append(pikachu)
+
+    pikachu_right = Pikachu_Right()
+    world.append(pikachu_right)
 
 def update_world():
     for o in world:
