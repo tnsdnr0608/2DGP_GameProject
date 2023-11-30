36a299d93bdeed9e6d7e490012026c086cbeb26e
Hong Soon Uk <tnsdnr1809@gmail.com>
2023-11-30 23:00:50 +0900

    피카츄의 정확한 속도 부여

--- StartGame/pikachu.py
+++ StartGame/pikachu.py
@@ -1,7 +1,14 @@
 from pico2d import load_image, SDL_KEYDOWN, SDL_KEYUP, SDLK_g, SDLK_d, SDLK_r, SDLK_SPACE, draw_rectangle
 
+import game_framework
 from ball import Ball
 import game_world
+
+PIXEL_PER_METER = (10.0 / 0.3)
+RUN_SPEED_KMPH = 20.0
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
 
 def right_down(e):
@@ -19,11 +26,14 @@ def left_down(e):
 def left_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_d
 
+
 def space_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
 
+
 def jump_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_r
+
 
 def jump_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_r
@@ -72,7 +82,8 @@ class Run:
     @staticmethod
     def do(pikachu):
         pikachu.frame = (pikachu.frame + 1) % 5
-        pikachu.x += pikachu.dir * 1
+        # pikachu.x += pikachu.dir * 1
+        pikachu.x += pikachu.dir * RUN_SPEED_PPS * game_framework.frame_time
 
     @staticmethod
     def draw(pikachu):
@@ -104,8 +115,8 @@ class StateMachine:
         self.pikachu = pikachu
         self.cur_state = Idle
         self.transitions = {
-            Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, space_down: Idle},
-            Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle, space_down: Run},
+            Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, space_down: Idle, jump_down: Jump},
+            Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle, space_down: Run, jump_up: Jump},
         }
 
     def start(self):
@@ -136,10 +147,10 @@ class Pikachu:
         self.face_dir = 1
         self.dir = 0
         self.image = load_image('walk_sheet.png')
+        # self.font = load_image('ENCR10B.TTF', 16)
         self.state_machine = StateMachine(self)
         self.state_machine.start()
         self.item = None
-
 
     def get_bb(self):
         return self.x - 50, self.y - 50, self.x + 50, self.y + 50
