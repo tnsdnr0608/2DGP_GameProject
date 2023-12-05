b6612f1bea355c3159b8e67bbe278920ea918c63
Hong Soon Uk <tnsdnr1809@gmail.com>
2023-12-06 07:02:11 +0900

    bgm 추가

--- StartGame/ground.py
+++ StartGame/ground.py
@@ -1,12 +1,17 @@
-from pico2d import load_image, draw_rectangle
+from pico2d import load_image, draw_rectangle, load_music
 import game_world
 import game_framework
 
 
 class Ground:
     def __init__(self):
+        self.font = None
         self.image = load_image('map.png')
         self.width, self.height = 800, 600
+        self.bgm = load_music('bgm.mp3')
+        self.bgm.set_volume(32)
+        self.bgm.repeat_play()
+
 
     def draw(self):
         self.image.draw(self.width // 2, self.height // 2)
